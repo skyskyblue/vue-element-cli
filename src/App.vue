459558f2a84c054ddecab24d08438fe3c09289c1
lvxinbo <lvxinbo258@163.com>
2019-12-17 15:49:17 +0800

    配置环境变量

--- src/App.vue
+++ src/App.vue
@@ -12,30 +12,10 @@
   export default {
     name: 'app',
     created() {
-      console.log('11')
     }
   }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
